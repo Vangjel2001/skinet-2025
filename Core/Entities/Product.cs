fee0a16195eac57fbb23c1fb6530c98cb57203bf
Vangjel Dhima <vdhima20@epoka.edu.al>
2025-09-14 18:03:59 +0200

    End of section 2

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Core.Entities;
+
+public class Product : BaseEntity
+{
+    public required String Name { get; set; }
+    public required String Description { get; set; }
+    public decimal Price { get; set; }
+    public required String PictureUrl { get; set; }
+    public required String Type { get; set; }
+    public required String Brand { get; set; }
+    public int QuantityInStock { get; set; }
+
+
+}
